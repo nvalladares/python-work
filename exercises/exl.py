dc51c61927d462d73a7f164f55d13107e69d9a5f
Nancy Valladares <nvalladares120@gmail.com>
2015-10-21 20:03:41 -0500

    committing exercise 2

--- exercises/exl.py
+++ exercises/exl.py
@@ -1,3 +1,4 @@
+# ------ Exercise 1
 print "Hello world!"
 print "Hello again"
 print "I like typing this."
@@ -5,3 +6,16 @@ print "This is fun."
 print 'Yay! printing.'
 print "I'd much rather You 'not'."
 print 'I "said" do not touch this.'
+print "Cats are awesome!"
+# print "cats are better than dogs!"
+
+# -------- Exercise 2
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
+
+print "This will run."
