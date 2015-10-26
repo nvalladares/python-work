39b1f569eeb031a462f21d4690b528b9edb70d5b
Nancy Valladares <nvalladares120@gmail.com>
2015-10-25 19:54:54 -0500

    committing exercise 5

--- exercises/exl.py
+++ exercises/exl.py
@@ -45,52 +45,70 @@
 # print "Is it greater or equal?", 5 >= -2
 # print "Is it less or equal?", 5 <= -2
 
-# -------- Exercise 4 - Variables and Names
+# -------- Exercise 4
 
 # variable 'cars' has a value of 100
-cars = 100
+# cars = 100
 
 # variable for 'space in a car' has a value of 4.0
-space_in_a_car = 4.0
+# space_in_a_car = 4.0
 
 # variable for 'drivers' has a value of 30
-drivers = 30
+# drivers = 30
 
 # variable for 'passengers' has a value of 90
-passengers = 90
+# passengers = 90
 
 # var 'cars not driven' value is the difference of 'cars' - 'drivers'
-cars_not_driven = cars - drivers
+# cars_not_driven = cars - drivers
 
 # var for 'cars driven' is equal to 'drivers'
-cars_driven = drivers
+# cars_driven = drivers
 
 # var 'carpool capacity' is the product of 'cars driven' * 'space in a car'
-carpool_capacity = cars_driven * space_in_a_car
+# carpool_capacity = cars_driven * space_in_a_car
 
 # var 'average of passengers per car' takes average of 'passengers' divided by 'cars driven'
-average_passengers_per_car = passengers / cars_driven
+# average_passengers_per_car = passengers / cars_driven
 
 # outputs value of var 'cars' (100)
-print "There are", cars, "cars available."
+# print "There are", cars, "cars available."
 
 # outputs value of var 'drivers' (30)
-print "There are only", drivers, "drivers available."
+# print "There are only", drivers, "drivers available."
 
 # outputs difference of 'cars' - 'drivers'
-print "There will be", cars_not_driven, "empty cars today."
+# print "There will be", cars_not_driven, "empty cars today."
 
 # outputs value of var 'carpool capacity' which is the product of 'cars driven' * 'space in a car'
-print "We can transport", carpool_capacity, "people today."
+# print "We can transport", carpool_capacity, "people today."
 
 # outputs value of var 'passengers' (90)
-print "We have", passengers, "to carpool today."
+# print "We have", passengers, "to carpool today."
 
 # outputs average of 'passengers' divided by 'cars driven'
-print "We need to put about", average_passengers_per_car, "in each car."
+# print "We need to put about", average_passengers_per_car, "in each car."
 
+# --------------- Exercise 5
 
+my_name = 'Frodo Baggins'
+my_age = 33 # when I left the Shire
+my_height = 42 # inches
+my_weight = 80 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
 
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on how close to Mordor he is." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight)
 
 
 
