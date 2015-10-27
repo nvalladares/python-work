dc14765b84917c1757fa6af6e2c96e4ee1ec718e
Nancy Valladares <nvalladares120@gmail.com>
2015-10-27 12:31:18 -0500

    added exercise 6

--- exercises/exl.py
+++ exercises/exl.py
@@ -1,39 +1,40 @@
-# # # ------ Exercise 1
-# # print "Hello world!"
-# # print "Hello again"
-# # print "I like typing this."
-# # print "This is fun."
-# # print 'Yay! printing.'
-# # print "I'd much rather You 'not'."
-# # print 'I "said" do not touch this.'
-# # print "Cats are awesome!"
-# # # print "cats are better than dogs!"
+# ------ Exercise 1
+# print "Hello world!"
+# print "Hello again"
+# print "I like typing this."
+# print "This is fun."
+# print 'Yay! printing.'
+# print "I'd much rather You 'not'."
+# print 'I "said" do not touch this.'
+# print "Cats are awesome!"
+# print "cats are better than dogs!"
 
-# # # -------- Exercise 2
-# # # A comment, this is so you can read your program later.
-# # # Anything after the # is ignored by python.
+# -------- Exercise 2
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
 
-# # print "I could have code like this." # and the comment after is ignored
+# print "I could have code like this." # and the comment after is ignored
 
-# # # You can also use a comment to "disable" or comment out a piece of code:
-# # # print "This won't run."
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run."
 
-# # print "This will run."
+# print "This will run."
 
-# # --------- Exercise 3
-# # prints "I will now count my chickens"
+# --------- Exercise 3
+# prints "I will now count my chickens"
 # print "I will now count my chickens:"
 
-# # adds 25+30 and divides the answer by 6 and prints the answer = 30
+# adds 25+30 and divides the answer by 6 and prints the answer = 30
 # print "Hens", 25 + 30 / 6
 
-# # * and % take precedence over -, so we first evaluate 25 * 3 % 4.  * and % have the same priority and associativity from left to right, so we evaluate from left to right, starting with 25 * 3. This yields 75. Now we evaluate 75 % 4, yielding 3. Finally, 100 - 3 is 97. 
-# print "Roosters", 100 - 25 * 3 % 4
+# * and % take precedence over -, so we first evaluate 25 * 3 % 4.  * and % have the same priority and associativity from left to right, so we evaluate from left to right, starting with 25 * 3. This yields 75. Now we evaluate 75 % 4, yielding 3. Finally, 100 - 3 is 97. 
 
-# # prints "Now I will count the eggs"
+#  print "Roosters", 100 - 25 * 3 % 4
+
+# prints "Now I will count the eggs"
 # print "Now I will count the eggs:"
 
-# # the answer is 7. The % (modulo) operator yields the remainder from the division of the first argument by the second. The numeric arguments are first converted to a common type. A zero right argument raises the ZeroDivisionError exception. The arguments may be floating point numbers, e.g., 3.14%0.7 equals 0.34 (since 3.14 equals 4*0.7 + 0.34.) The modulo operator always yields a result with the same sign as its second operand (or zero); the absolute value of the result is strictly smaller than the absolute value of the second operand [2].
+# the answer is 7. The % (modulo) operator yields the remainder from the division of the first argument by the second. The numeric arguments are first converted to a common type. A zero right argument raises the ZeroDivisionError exception. The arguments may be floating point numbers, e.g., 3.14%0.7 equals 0.34 (since 3.14 equals 4*0.7 + 0.34.) The modulo operator always yields a result with the same sign as its second operand (or zero); the absolute value of the result is strictly smaller than the absolute value of the second operand [2].
 # print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 # print "Is it true that 3 + 2 < 5 - 7?"
 # print 3 + 2 < 5 - 7
@@ -91,36 +92,61 @@
 
 # --------------- Exercise 5
 
-name = 'Frodo Baggins'
-age = 33 # when I left the Shire
-height = 42 # inches
-weight = 80 # lbs
-eyes = 'Blue'
-teeth = 'White'
-hair = 'Brown'
+# name = 'Frodo Baggins'
+# age = 33 # when I left the Shire
+# height = 42 # inches
+# weight = 80 # lbs
+# eyes = 'Blue'
+# teeth = 'White'
+# hair = 'Brown'
 
-print "Let's talk about %s." % name
-print "He is %d years old." % age 
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
-print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (eyes, hair)
-print "His teeth are usually %s depending on how close to Mordor he is." % teeth
+# print "Let's talk about %s." % name
+# print "He is %d years old." % age 
+# print "He's %d inches tall." % height
+# print "He's %d pounds heavy." % weight
+# print "Actually that's not too heavy."
+# print "He's got %s eyes and %s hair." % (eyes, hair)
+# print "His teeth are usually %s depending on how close to Mordor he is." % teeth
 
-# this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d." % (
-    age, height, weight, age + height + weight)
+# print "If I add %d, %d, and %d I get %d." % (
+#     age, height, weight, age + height + weight)
 
-print "Height converted from inches to centimeters is %d" % (height * 2.54)
+# print "Height converted from inches to centimeters is %d" % (height * 2.54)
 
-print "Weight converted from pounds to kilograms is %d" % (weight / 2.2)
+# print "Weight converted from pounds to kilograms is %d" % (weight / 2.2)
 
-print round(1.98999) 
+# print round(1.98999) 
 
 # These are format specifiers. The %d specifier refers specifically to a decimal (base 10) integer. The %s specifier refers to a Python string. It is used for string formatting
 
 # --------------- Exercise 6
 
+# var 'x' = 'there are 10 types of people'
+x = "There are %d types of people." % 10
+# var 'binary' = 'binary'
+binary = "binary"
+# var 'do_not' = 'don't
+do_not = "don't"
+# var 'y' = 'Those who understand binary and those who don't'
+y = "Those who know %s and those who %s." % (binary, do_not)
+# prints value of 'x'
+print x
+# prints value of 'y'
+print y
+
+# 
+print "I said: %r." % x
+print "I also said: '%s'." % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
 
 
 
